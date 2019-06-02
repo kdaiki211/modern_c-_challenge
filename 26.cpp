a735fbf155e377e340d12f622950e2a26f10f7ef
Daiki Komatsuda <kdaiki211@gmail.com>
2019-06-02 21:46:21 +0900

    add testcase of 26

--- 26.cpp
+++ 26.cpp
@@ -18,7 +18,9 @@ string concat(vector<string> str, char delimiter)
 
 int main(void)
 {
-  vector<string> str = { "this", "is", "an", "example" };
+  vector<string> str0 = { "this", "is", "an", "example" };
+  vector<string> str1;
   char delimiter = ' ';
-  cout << "\"" << concat(str, delimiter) << "\"" << endl;
+  cout << "\"" << concat(str0, delimiter) << "\"" << endl;
+  cout << "\"" << concat(str1, delimiter) << "\"" << endl;
 }
