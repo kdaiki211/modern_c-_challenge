1fb7d3161b274191863e65496bb5fa5fefc34e2d
Daiki Komatsuda <kdaiki211@gmail.com>
2019-07-02 01:31:53 +0900

    add 42

---
+++ 5/42.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <chrono>
+#include "../external/date/include/date/date.h"
+#include "../external/date/include/date/iso_week.h"
+
+using namespace std;
+using namespace date;
+using namespace iso_week;
+
+unsigned get_nth_day(sys_days sd)
+{
+  return (sd - static_cast<sys_days>(January/0/year_month_day{sd}.year())).count();
+}
+
+unsigned get_nth_week(sys_days sd)
+{
+  return (unsigned)year_weeknum_weekday{sd}.weeknum();
+}
+
+int main(void)
+{
+  sys_days d = July/2/2019;
+  cout << get_nth_day(d) << endl;
+  cout << get_nth_week(d) << endl;
+}
